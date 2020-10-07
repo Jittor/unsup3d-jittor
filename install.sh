57242c96b1bf9c2d069ba04dcea62f4a06e3581a
Jittor <59786298+Jittor@users.noreply.github.com>
2020-10-07 18:00:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/zhouwy19/N3MR_jittor.git
-cp -r N3MR_jittor/neural_renderer ./
+git clone https://github.com/Jittor/n3mr-jittor
+cp -r n3mr-jittor/neural_renderer ./
 mkdir init_models
 wget https://cg.cs.tsinghua.edu.cn/jittor/assets/build/vgg_pretrained_features.pkl
 mv vgg_pretrained_features.pkl init_models/vgg_pretrained_features.pkl
